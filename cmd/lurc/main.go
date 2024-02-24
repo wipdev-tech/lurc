fe5e9da9af0dc461c69032d57be52872e4a4de0e
wipdev-tech <wipdev.tech@gmail.com>
2024-02-24 16:39:49 +0200

    Reading user input

--- cmd/lurc/main.go
+++ cmd/lurc/main.go
@@ -8,10 +8,18 @@ import (
 
 var styles = struct {
 	header l.Style
+	prompt l.Style
 }{
 	header: l.NewStyle().Background(l.Color("56")).Bold(true),
+	prompt: l.NewStyle().Foreground(l.Color("248")),
 }
 
 func main() {
 	fmt.Printf("\n%s\n\n", styles.header.Render(" lurc "))
+
+	url := ""
+	fmt.Printf(styles.prompt.Render("URL: "))
+	fmt.Scan(&url)
+
+	fmt.Println(url)
 }
